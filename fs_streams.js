b0927f6101362e9274e08144926e491d50d6a8c9
Kailie Field <kailie.field@gmail.com>
2025-01-16 18:40:48 -0500

    updating to include word steam update

--- fs_streams.js
+++ fs_streams.js
@@ -1,9 +1,7 @@
 const fs = require('fs')
-
 const zlib = require('zlib')
-
-const ToLowerCasePipe = require ('./word_split_pip')
-
+const ToLowerCasePipe = require('./word_split_pip')
+const ToWordsPipe = require('./word_split_pip')
 
 const readStream = fs.createReadStream('input_stream.txt')
 const writeStream = fs.createWriteStream('output_stream.txt')
@@ -23,15 +21,23 @@ readStream.on('error', (err) =>{
 readStream.on('close', ()=> {
     console.log('read stream close')
 })
-writeStream.on('error', ()=> {
+
+writeStream.on('error', (err) =>{
     console.log(`error write stream ${err}`)
 })
 
 writeStream.write("Hello")
 writeStream.write("World")
 writeStream.write("NodeJs")
+// writeStream.end()
 
 //pipes
 readStream.pipe(writeStream)
 readStream.pipe(zlib.createGzip()).pipe(fs.createWriteStream('stream.gz'))
-readStream.pipe( new ToLowerCasePipe()).pipe(fs.createWriteStream('lower_stream.txt'))
+
+readStream.pipe(new ToLowerCasePipe())
+            .pipe(fs.createWriteStream('lower_stream.txt'))
+
+
+readStream.pipe(new ToWordsPipe())
+            .pipe(fs.createWriteStream('words_stream.txt'))
--- word_split_pip.js
+++ word_split_pip.js
@@ -1,14 +1,12 @@
 const { Transform } = require('stream');
 
-class ToLowerCasePipe extends Transform {
-
-    constructor(){
-        super()
-        this._transform = (chunk, encoding, cb) =>{
-            cb(null, chunk.toString().toLowerCase())
-
-        }
-    }
+class ToWordsPipe extends Transform {
+   constructor() {
+      super()
+      this._transform = (chunk, encoding, cb) => {
+         cb(null, chunk.toString().replaceAll(' ', "\r\n"))
+      }
+   }
 }
 
-module.exports = ToLowerCasePipe
+module.exports = ToWordsPipe
