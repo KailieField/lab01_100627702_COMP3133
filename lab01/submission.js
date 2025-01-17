a4b9984fdf97008ba967d8ee093cea95ba73dc29
Kailie Field <kailie.field@gmail.com>
2025-01-16 21:09:09 -0500

    updating and fixing code for successful completion

--- lab01/submission.js
+++ lab01/submission.js
@@ -6,29 +6,33 @@ if (fs.existsSync('canada.txt')) fs.unlinkSync('canada.txt');
 if (fs.existsSync('usa.txt')) fs.unlinkSync('usa.txt');
 
 // -- reading 
-fs.readFileSync('./input_countries.csv', 'utf-8', (err, data) => {
-    if(err){
-    console.error('--- ERROR : ', err);
-    return;
-    }
+try { 
 
+    const data = fs.readFileSync('input_countries.csv', 'utf-8');
     // -- formatting header & lines to transcribe
-    const header = lines[0];
     const lines = data.split('\n');
-
+    const header = lines[0];
     // -- filtering and extracting canadian and american data
     const canadianData = lines.filter((line) => line.startsWith('Canada'));
     const americanData = lines.filter((line) => line.startsWith('United States'));
 
     // -- writing canadian data to canada.txt
     if(canadianData.length > 0) {
+
         fs.writeFileSync('canada.txt', [header, ...canadianData].join('\n'));
         console.log('--- transcribed to canadian.txt');
     }
 
     // -- writing american data to usa.txt
     if (americanData.length > 0) {
+
         fs.writeFileSync('usa.txt', [header, ...americanData].join('\n'));
         console.log('--- transribed to usa.txt');
     }
-});
+
+} catch(err){
+
+    console.error(" --- ERROR: ", err);
+}
+
+console.log(" --- END --- ");
